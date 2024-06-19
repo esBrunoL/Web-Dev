69b4b0455248712c52713c6ae761945ab4f358b2
Bruno-Lobo <brunoaglobo@gmail.com>
2024-06-19 19:35:39 -0400

    this is the first commit of the day

---
+++ Welcome.js
@@ -0,0 +1,15 @@
+let welcomeM = "welcome to Bruno's read-me";
+alert (welcomeM);
+
+let Name = "Lua"; let age = "25"; let sales = "50"; let Balance ="1000"; let Emessage = "Error! Please fulfill the form"; let Mage ="18";
+
+alert (Emessage);
+let Name = promt("please enter your name:");
+let Age = prompt("Please enter your age:");
+
+if(Age > Mage) {
+    alert("You may have a driver's license!");
+}
+else{
+    alert ("Sorry, but You can't have a Driver's license");
+}
