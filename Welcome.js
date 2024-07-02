3142651f41853eff92aae0e3b5b0243362326085
Bruno-Lobo <brunoaglobo@gmail.com>
2024-07-02 08:24:01 -0400

    first commit of the day

--- Welcome.js
+++ Welcome.js
@@ -1,15 +1,20 @@
 let welcomeM = "welcome to Bruno's read-me";
 alert (welcomeM);
 
-let Name = "Lua"; let age = "25"; let sales = "50"; let Balance ="1000"; let Emessage = "Error! Please fulfill the form"; let Mage ="18";
+let Name = "Lua"; 
+let age = "25"; 
+let sales = "50"; 
+let Balance ="1000"; 
+let Emessage = "Error! Please fulfill the form"; 
+let Mage ="18";
 
 alert (Emessage);
-let Name = promt("please enter your name:");
-let Age = prompt("Please enter your age:");
+Name = prompt("please enter your name:");
+Age = prompt("Please enter your age:");
 
-if(Age > Mage) {
-    alert("You may have a driver's license!");
+if(Age >= Mage) {
+    alert("Congratulations, You may have a driver's license!");
 }
 else{
-    alert ("Sorry, but You can't have a Driver's license");
+    alert ("Sorry , but You can't have a Driver's license");
 }
